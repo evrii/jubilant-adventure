9b66db9860e1cbc16df8a3de182f14e09911044d
Rollins, Eric Van <erollins3@gatech.edu>
2021-12-18 10:11:19 -0500

    Added skeleton for RouteService tests.

---
+++ src/test/java/com/navigator/services/RouteServiceTest.java
@@ -0,0 +1,23 @@
+package com.navigator.services;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class RouteServiceTest {
+
+    @Test
+    void testThatGetRoutesReturnsTheCorrectListOfRoytes(){
+        assertTrue(false);
+    }
+
+    @Test
+    void testConvertJsonArrayToRouteList(){
+        assertTrue(false);
+    }
+
+    @Test
+    void convertJsonObjectToRoute(){
+        assertTrue(false);
+    }
+    
+}
